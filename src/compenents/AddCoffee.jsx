e005527f321ae215f4c402d858a23288bb984c07
dev-abulhassan <abulhassan.dev@gmail.com>
2023-05-12 17:21:49 +0600

    add something

--- src/compenents/AddCoffee.jsx
+++ src/compenents/AddCoffee.jsx
@@ -8,6 +8,11 @@ const AddCoffee = () => {
                <h2>Add Coffee</h2>
                <p className=''>It is a long established fact that a reader will be distraceted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here.</p>
             </div>
+            <div>
+               <form>
+
+               </form>
+            </div>
          </div>
       </div>
    );
